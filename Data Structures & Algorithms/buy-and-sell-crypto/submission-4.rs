917b6a92112db821a823dfd0cd1b9ee17e89abc0
Tanmay Patil <patil.tanmay0697@gmail.com>
2026-08-21 02:43:05 -0500

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut mp = 0;
+        let n = prices.len();
+
+        let mut l = 0;
+        let mut r = 0;
+        while r < prices.len() {
+            if prices[l] < prices[r] {
+                let profit = prices[r] - prices[l];
+                mp = mp.max(profit);
+            } else {
+                l = r;
+            }
+            r += 1;
+        }
+        mp
+    }
+}
